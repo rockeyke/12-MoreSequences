269417c8dd8e7964ac63a6562fbf6469a64451ed
rockeyke <rockeyke@rose-hulman.edu>
2018-12-20 14:59:38 -0500

    Done

--- src/m1_pizza.py
+++ src/m1_pizza.py
@@ -563,7 +563,7 @@ def fancy_polygon(window, circle, number_of_lines, hops_to_next_point, color,
       :type thickness:       int
     """
     # -------------------------------------------------------------------------
-    # TODO: 10. Implement and test this function.
+    # DONE: 10. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     #
     # IMPLEMENTATION REQUIREMENT:
